5820de53073d851d23c8d376234477654afbcacf
Jamal KHALIS <39938150+jamalkhalis@users.noreply.github.com>
2022-02-15 01:22:54 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,36 @@
-function detectWidth(x) {
-  if (width.matches) { // If media query matches
-    document.getElementById('header').classList.add("navbar-dark")
-    document.getElementsByTagName('header')[0].classList.add("bg-transparent")
-    document.getElementById('navbar-nav-id').classList.add("bg-transparent")
-    document.getElementById('navbar-nav-id').classList.add("mt-0")
-  } else {
-    document.getElementById('header').classList.add("navbar-light")
-    document.getElementsByTagName('header')[0].classList.add("bg-light")
-    document.getElementById('navbar-nav-id').classList.add("bg-white")
-    document.getElementById('navbar-nav-id').classList.add("mt-3")
+function detectWidth(width) {
+  if (width >= 992) { // If media query matches
+    document.getElementById('headerId').classList.remove("navbar-light");
+    document.getElementById('headerId').classList.add("navbar-dark");
 
+    document.getElementsByTagName('header')[0].classList.remove("bg-light");
+    document.getElementsByTagName('header')[0].classList.add("bg-transparent");
+
+    document.getElementById('navbar-nav-id').classList.remove("bg-white");
+    document.getElementById('navbar-nav-id').classList.remove("mt-3");
+
+    document.getElementById('navbar-nav-id').classList.add("bg-transparent");
+    document.getElementById('navbar-nav-id').classList.add("mt-0");
+
+  } else {
+    document.getElementById('headerId').classList.remove("navbar-dark");
+    document.getElementById('headerId').classList.add("navbar-light");
+
+    document.getElementsByTagName('header')[0].classList.remove("bg-transparent");
+    document.getElementsByTagName('header')[0].classList.add("bg-light");
+
+    document.getElementById('navbar-nav-id').classList.remove("bg-transparent");
+    document.getElementById('navbar-nav-id').classList.remove("mt-0");
+
+    document.getElementById('navbar-nav-id').classList.add("bg-white");
+    document.getElementById('navbar-nav-id').classList.add("mt-3");
   }
 }
 
-var width = window.matchMedia("(min-width: 992px)")
-detectWidth(width) // Call listener function at run time
-width.addListener(detectWidth) // Attach listener function on state changes
-console.log(width)
+var newWidth = window.innerWidth;
+detectWidth(newWidth);// Call function at run time
+
+window.addEventListener('resize', function(event){
+    detectWidth(window.innerWidth);
+    console.log(window.innerWidth);
+});
