de063e0fa8393c0396785318f0449c6f7e031393
Jamal KHALIS <39938150+jamalkhalis@users.noreply.github.com>
2022-02-16 22:42:17 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,22 @@ window.addEventListener('resize', function(event){
     detectWidth(window.innerWidth);
     console.log(window.innerWidth);
 });
+
+
+// carousel
+
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 6
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+          next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
