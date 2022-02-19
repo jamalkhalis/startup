80652fc5cc1c80022872fa7716f2113c307baabd
Jamal KHALIS <39938150+jamalkhalis@users.noreply.github.com>
2022-02-19 22:07:09 +0100

    update!!!!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var colorTwo = "#0e305d";
+
 function detectWidth(width) {
   if (width >= 992) { // If media query matches
     document.getElementById('headerId').classList.remove("navbar-light");
@@ -61,7 +63,7 @@ function hideCollapseNavbar(hideNavbar) {
 
   hideNavbar.addEventListener('click', function (event) {
     document.getElementById("buttonNavbarToggler").ariaExpanded = "false";
-    collapseNavbar.classList.remove("show");
+    collapseNavbar.classList.remove("show");    
   })
 
 }
@@ -75,20 +77,78 @@ hideCollapseNavbar(hideNavbar[i]);
 // Detect click outside of navbar-collabse and hide it
 window.addEventListener('click', function(e){ 
 
-  if (document.getElementById('navbarSupportedContent').contains(e.target)){
+  var collapsingNavbar = document.getElementById('navbarSupportedContent');
+
+  if (collapsingNavbar.contains(e.target)){
     // Clicked in box (navbar-toggler)
-
   } else{
-
-    // Clicked out bx (navbar-toggler)
-    document.getElementById("navbarSupportedContent").classList.remove("show");
-    // document.getElementById("buttonNavbarToggler").ariaExpanded = "false";
+    
+    // collapsingNavbar.classList.remove("show");
 
   }
-
+  
 });
 
 var svg = document.getElementsByClassName("svg-testimonials");
 for (var i = svg.length - 1; i >= 0; i--) {
   svg[i].setAttribute("fill", "var(--four)");
 }
+
+// var buttonCardServices = document.getElementById('collapseButton');
+// buttonCardServices.addEventListener('click', function(event){
+//   document.getElementById('collapseExampleOverlay').classList.add("bg-color-two");
+// })
+
+function hexToRGB(hex, alpha) {
+    var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+
+    if (alpha) {
+        return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+    } else {
+        return "rgb(" + r + ", " + g + ", " + b + ")";
+    }
+}
+
+// hexToRGB('#FF0000', 0.5);
+
+// Services element
+
+function collapseServices(myCollapsible, collapseServicesOverlay) {
+
+  myCollapsible.addEventListener('show.bs.collapse', function () {
+    collapseServicesOverlay.style.backgroundColor = hexToRGB(colorTwo, 0.5);
+  })
+
+  myCollapsible.addEventListener('hidden.bs.collapse', function () {
+    collapseServicesOverlay.style.backgroundColor = '';
+
+  })
+
+}
+var collapseServicesOverlay1 = document.getElementById('collapseServicesOverlay1');
+var collapseServices1 = document.getElementById('collapseServices1');
+collapseServices(collapseServices1, collapseServicesOverlay1);
+
+var collapseServicesOverlay2 = document.getElementById('collapseServicesOverlay2');
+var collapseServices2 = document.getElementById('collapseServices2');
+collapseServices(collapseServices2, collapseServicesOverlay2);
+
+var collapseServicesOverlay3 = document.getElementById('collapseServicesOverlay3');
+var collapseServices3 = document.getElementById('collapseServices3');
+collapseServices(collapseServices3, collapseServicesOverlay3);
+
+var cardHover = document.getElementById('cardHover');
+
+cardHover.addEventListener('mouseover', function () {
+  document.getElementById('collapseServices1').classList.add("show")
+  document.getElementById('collapseServicesOverlay1').style.backgroundColor = hexToRGB(colorTwo, 0.5);
+
+})
+
+cardHover.addEventListener('mouseleave', function () {
+  document.getElementById('collapseServices1').classList.remove("show")
+  document.getElementById('collapseServicesOverlay1').style.backgroundColor = "";
+
+})
